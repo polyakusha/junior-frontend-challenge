de49f555dddacc83790f5bfb07c2a999c39c5f8c
Sergey Poliakov <ps.force@gmail.com>
2017-05-30 14:34:50 +0200

    Hours, minutes and seconds

--- js/timer.js
+++ js/timer.js
@@ -1,7 +1,30 @@
 function startTimer(time, timerSelector) {
     var timerCount = time;
+    var hours;
+    var minutes;
+    var seconds;
     var actualTimer = setInterval(function () {
-        timerSelector.innerHTML = timerCount;
+        minutes = parseInt(timerCount / 60, 10);
+        seconds = parseInt(timerCount % 60, 10);
+        if (minutes >= 60) {
+            hours = parseInt(minutes / 60, 10);
+            minutes = parseInt(minutes % 60, 10);
+        }
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        if (hours < 10) {
+            hours = "0" + hours;
+        }
+        if (hours) {
+            timerSelector.innerHTML = hours + ":" + minutes + ":" + seconds;
+        }
+        else {
+            timerSelector.innerHTML = minutes + ":" + seconds;
+        }
         timerCount--;
         if (timerCount < 0) {
             clearInterval(actualTimer);
@@ -10,7 +33,7 @@ function startTimer(time, timerSelector) {
 }
 
 window.onload = function () {
-    var time = 10;
+    var time = 33333;
     var timerSelector = document.getElementById('timer');
     startTimer(time, timerSelector);
 };
